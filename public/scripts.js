46966f446f5705857f368374eeaca7efbb05f7c7
maddrings <mrings@email.unc.edu>
2023-05-09 21:14:26 -0400

    Add to scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,52 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function showHideShots() {
+    let opponent = document.getElementById('opponent');
+    let rps = document.getElementById('rps');
+    
+    if (opponent.checked == true){
+        console.log("opponent.checked == true && radio_rpsls.checked == true");
+        $('.shots').show();
+        
+        if (rps.checked == true){
+            $('.rpsls').hide();
+        }
+    } else {
+        $('.shots').hide();
+    }
+
+}
+
+function startOver() {
+    document.getElementById('userinput').reset();
+    showHideShots();
+}
+
+async function playGame() {
+    console.log('play game is being called');
+
+    let opponent = document.getElementById('opponent');
+
+    let game = $('input[type=radio][name=game]:checked').val();
+    console.log('game', game);
+
+    let shot = ''
+    if (opponent.checked == true){
+        shot = $('input[type=radio][name=shot]:checked').val();
+        console.log('shot', shot);
+    }
+
+    let baseurl = window.location.href + 'app/'
+    console.log(baseurl);
+    let url = baseurl + game + '/play/' + shot
+    console.log('url:', url);
+    let response = await fetch(url);
+    console.log('response:', response);
+    let result = await response.json();
+    console.log('result:', result);
+
+    const myDiv = document.getElementById("answer");
+    myDiv.innerHTML = "Player: "+result.player+" Opponent: "+result.opponent+"\n\nResult: "+result.result;
+}
